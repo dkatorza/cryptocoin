cf56602b28d021eb92ff7939d21a36fe7f32c234
Dan Katorza <dkatorza@gmail.com>
2021-12-07 22:29:22 +0200

    build

---
+++ dist/precache-manifest.19c17c5b6b46164ca9b3d5845b39c921.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b17d66d114899e6ae28",
+    "url": "/css/app.32c0fce6.css"
+  },
+  {
+    "revision": "a897abf020bbb8ec69a86cfcfd402c96",
+    "url": "/img/livestatus.a897abf0.svg"
+  },
+  {
+    "revision": "95929e879e488eea0debea7b3b0fa31c",
+    "url": "/img/mrbitcoin-logo.95929e87.png"
+  },
+  {
+    "revision": "67f7275fdddb208a82a4f8d831a97929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b17d66d114899e6ae28",
+    "url": "/js/app.42cb383d.js"
+  },
+  {
+    "revision": "a5610cdccd717fe7fe94",
+    "url": "/js/chunk-vendors.f2e96567.js"
+  },
+  {
+    "revision": "3b7a856f1f5aa97ba5ec6562a58d8990",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
